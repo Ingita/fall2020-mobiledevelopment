3f44acedf846666c4641565c923ff7af0d1e4c51
Ingita <ingam8@gmail.com>
2020-10-06 17:52:38 -0400

    week6

---
+++ week6/src/components/Example/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const Example = () =>{
+return (
+    <p>Hi! My name is Inga Moiseienko</p>
+)
+};
+
+export default Example;
+
+
+
+// class Example extends Component {
+//  render(){
+//      return(
+//         <p>Hi! my name is Inga Moiseienko</p>
+//      )
+//  }
+// }
